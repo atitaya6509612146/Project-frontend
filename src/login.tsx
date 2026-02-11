c18bc69f4698b682bd510b35da5e208152dbc000
atitaya6509612146 <atitaya.upa@dome.tu.ac.th>
2026-02-11 20:35:53 +0700

    update login page

---
+++ src/login.tsx
@@ -0,0 +1,56 @@
+import { Button, ConfigProvider, Form, Input } from 'antd'
+import './login.css'
+
+function Login() {
+  const handleFinish = (values: { username: string; password: string }) => {
+    console.log('login submit', values)
+  }
+
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#47555b',
+          borderRadius: 12,
+          fontFamily: "'SukhumvitSet', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+        },
+      }}
+    >
+      <div className="login-page">
+        <header className="login-header">logo</header>
+
+        <main className="login-panel">
+          <h1 className="login-title">LOGIN</h1>
+
+          <Form
+            className="login-form"
+            layout="vertical"
+            requiredMark={false}
+            onFinish={handleFinish}
+          >
+            <Form.Item name="username" rules={[{ required: true, message: 'Please enter username' }]}> 
+              <Input size="large" placeholder="Username" />
+            </Form.Item>
+
+            <Form.Item name="password" rules={[{ required: true, message: 'Please enter password' }]}> 
+              <Input.Password size="large" placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item className="login-submit-wrap">
+              <Button type="primary" htmlType="submit" className="login-submit-btn">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <div className="login-alt">
+            <span>or</span>
+            <a href="#">Sign up</a>
+          </div>
+        </main>
+      </div>
+    </ConfigProvider>
+  )
+}
+
+export default Login
